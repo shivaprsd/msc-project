b4bcac313cc647fff28f8df3bc789134ade169a4
Shiva Prasad <shivaprsdv@gmail.com>
2019-12-03 09:21:16 +0530

    Add adjacency list handling functions

--- network.c
+++ network.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "network.h"
 #include "util.h"
+#define HASH(n) (n - 1)
+
+struct adjnode *install(struct adjnode **node, int id, int wt)
+{
+	struct adjnode *ap;
+
+	ap = (struct adjnode *) malloc(sizeof(*ap));
+	if (ap != NULL) {
+		ap->id = id;
+		ap->wt = wt;
+		ap->next = *node;
+		*node = ap;
+	}
+	return ap;
+}
+
+int readadjl(struct adjnode **adjlist, int lim)
+{
+	int n1, n2, w, edgc;
+
+	edgc = 0;
+	while (scanf("%d %d %d", &n1, &n2, &w) != EOF) {
+		if (n1 > lim || n2 > lim) {
+			fprintf(stderr, "Node %d is out of limits!\n",
+					(n1 > lim) ? n1 : n2);
+			return -1;
+		}
+		++edgc;
+		if (install(&adjlist[HASH(n1)], n2, w) == NULL
+				|| install(&adjlist[HASH(n2)], n1, w) == NULL) {
+			fprintf(stderr, "Memory allocation error!\n");
+			return -1;
+		}
+	}
+	return edgc;
+}
+
+void printadjl(struct adjnode **adjlist, int lim)
+{
+	struct adjnode *ap;
+	int i;
+
+	for (i = 0; i < lim; ++i) {
+		printf("%d:", i + 1);
+		for (ap = adjlist[i]; ap != NULL; ap = ap->next)
+			printf(" %d (%d)%s", ap->id, ap->wt,
+				(ap->next != NULL) ? "," : "");
+		putchar('\n');
+	}
+}
+
+void degdist(struct adjnode **adjlist, int lim)
+{
+	struct adjnode *ap;
+	int i, d;
+
+	for (i = 0; i < lim; ++i) {
+		for (d = 0, ap = adjlist[i]; ap != NULL; ap = ap->next, ++d)
+			;
+		printf("%d %d\n", i + 1, d);
+	}
+}
 
 void init_ring(int *ring, int n)
 {
--- network.h
+++ network.h
@@ -1,6 +1,16 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+struct adjnode {
+	struct adjnode *next;
+	int id;
+	int wt;
+};
+struct adjnode *install(struct adjnode **node, int id, int wt);
+int readadjl(struct adjnode **adjlist, int lim);
+void printadjl(struct adjnode **adjlist, int lim);
+void degdist(struct adjnode **adjlist, int lim);
+
 void init_ring(int *ring, int n);
 void plot_ring(int *ring, int n);
 int adj(int *ring, int i, int j);
