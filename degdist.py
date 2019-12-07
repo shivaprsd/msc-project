de7955c4f9999466979ff364c78f3610c1a128d3
Shiva Prasad <shivaprsdv@gmail.com>
2019-12-07 11:41:56 +0530

    Modify script to output actual degree distribution
    Till now we were printing degree vs node.

--- degdist.py
+++ degdist.py
@@ -3,7 +3,9 @@ f = open(sys.argv[-1], "r")
 g = [[int(x) for x in l.split(" ")] for l in f.readlines()]
 f.close()
 n = max(max(l[0] for l in g), max(l[1] for l in g))
+p = {}
 
+print "#i k1 k2 k"
 for i in range(1, n + 1):
     k1 = k2 = 0
     for j in range(len(g)):
@@ -12,4 +14,11 @@ for i in range(1, n + 1):
                 k1 += 1
             else:
                 k2 += 1
-    print i, k1, k2
+    k = k1 + k2
+    print i, k1, k2, k
+    if k not in p.keys():
+        p[k] = 0
+    p[k] += 1
+print "\n\n#k P(k)"
+for k in sorted(p.keys()):
+    print k, p[k]
