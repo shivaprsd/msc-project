0dcbad1edefdbc7c1f198d6e7eced2f9a169646e
Shiva Prasad <shivaprsdv@gmail.com>
2020-06-29 19:42:52 +0530

    Compute max |r|-fluctuations for diff Leader freq

--- kuramoto.c
+++ kuramoto.c
@@ -5,9 +5,9 @@
 #include "util.h"
 #define N 1000
 
-double omega[N];
+double omega[N], om0;
 struct adjnode *adjlist[N] = { NULL };
-#pragma omp threadprivate(omega)
+#pragma omp threadprivate(om0)
 
 void kuramoto(double t, double theta[N], double thdot[N])
 {
@@ -19,6 +19,7 @@ void kuramoto(double t, double theta[N], double thdot[N])
 		for (ap = adjlist[i]; ap != NULL; ap = ap->next)
 			thdot[i] += ap->wt * sin(theta[ap->id - 1] - theta[i]);
 	}
+	thdot[0] = om0;
 }
 
 double ord_param(double *theta, unsigned int n)
@@ -45,39 +46,33 @@ void print_osc(double *theta, unsigned int n)
 int main(int argc, char *argv[])
 {
 	int t, i, j;
-	const int T = 1e4;
+	const int T = 5e4;
 	char s[13];
 	double r, h, theta[N], theta0[N];
 	const double sigma = M_PI / 2;
 	FILE *fp;
 
 	fp = fopen(argv[argc - 1], "r");
-	for (i = 0; i < N; ++i)
+	for (i = 0; i < N; ++i) {
 		fscanf(fp, "%le\t%le\n", &theta0[i], &omega[i]);
+		omega[i] -= 2 * M_PI;
+	}
 	fclose(fp);
 	readadjl(adjlist, N, DIR);
 	h = 1e-3;
 
 #pragma omp parallel for private(i, t, r, s, fp, theta)
-	for (j = 0; j < 400; ++j) {
-		sprintf(s, "omega%.3d.dat", j + 1);
-		fp = fopen(s, "w");
-		for (i = 0; i < N; ++i) {
-			omega[i] = gauss(2 * M_PI, sigma);
-			fprintf(fp, "%d\t%.17le\n", i + 1, omega[i]);
-			fflush(stdout);
-		}
-		fclose(fp);
-		//puts("# Time (a.u.)\tOrder parameter |r|");
-		//printf("\"g(w): std dev = %.3lf\"\n", sigma);
+	for (j = 0; j <= 400; ++j) {
+		om0 = (j - 200) * M_PI / 200;
 		copy_vec(theta0, theta, N);
 		for (t = 0; t < T; ++t)
 			rk4(t * h, theta, N, kuramoto, h, theta);
 		for (t = 0; t < T; ++t) {
-			r = ord_param(theta, N);
-			if (!(t % 100))
+			if (!(t % 50)) {
+				r = ord_param(theta, N);
 #pragma omp critical
-				printf("%d\t%lf\n", j + 1, r);
+				printf("%d\t%lf\t%lf\n", j, r, t * h);
+			}
 			rk4(t * h, theta, N, kuramoto, h, theta);
 		}
 	}
